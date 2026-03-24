53ab5a095474f13e0f3f6d3339a7b14805c54fb1
kikelopezz <128167221+kikelopezz@users.noreply.github.com>
2026-03-24 17:46:10 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // Solo para desarrollo local
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host:     process.env.MYSQLHOST || 'mysql.railway.internal', // Host real
+  host:     process.env.MYSQLHOST || 'mysql://root:QHNicTgIkdYKsrzSveirfeAmrYELHsQD@centerbeam.proxy.rlwy.net:46264/railway', // Host real
   port:     process.env.MYSQLPORT || 3306,
   database: process.env.MYSQLDATABASE || 'railway',
   user:     process.env.MYSQLUSER || 'root',
